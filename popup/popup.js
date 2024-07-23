dbd358cc3cb02c0fa4386e8a9b0f749f0efac92d
Anubhav <mr.anubhavtiwary@gmail.com>
2024-07-23 15:26:16 +0530

    added reset

--- popup/popup.js
+++ popup/popup.js
@@ -1,7 +1,62 @@
-const tasks = [];
+let tasks = [];
+
+function updateTime() {
+  chrome.storage.local.get(["timer"], (res) => {
+    const time = document.getElementById("time");
+    const minutes = `${25 - Math.ceil(res.timer / 60)}`.padStart(2, "0");
+    let seconds = "00";
+    if (res.timer % 60 != 0) {
+      seconds = `${60 - (res.timer % 60)}`.padStart(2, "0");
+    }
+    time.textContent = `${minutes}:${seconds}`;
+  });
+}
+
+updateTime();
+setInterval(updateTime, 1000);
+
+const startTimerBtn = document.getElementById("start-timer-btn");
+startTimerBtn.addEventListener("click", () => {
+  chrome.storage.local.get(["isRunning"], (res) => {
+    chrome.storage.local.set(
+      {
+        isRunning: !res.isRunning,
+      },
+      () => {
+        startTimerBtn.textContent = !res.isRunnning
+          ? "Pause Timer"
+          : "Start Timer";
+      }
+    );
+  });
+});
+
+const resetTimerBtn = document.getElementById("reset-timer-btn");
+resetTimerBtn.addEventListener("click", () => {
+  chrome.storage.local.set(
+    {
+      timer: 0,
+      isRunning: false,
+    },
+    () => {
+      startTimerBtn.textContent = "Start Timer";
+    }
+  );
+});
 
 const addTaskBtn = document.getElementById("add-task-btn");
 addTaskBtn.addEventListener("click", () => addTask());
+
+chrome.storage.sync.get(["tasks"], (res) => {
+  tasks = res.tasks ? res.tasks : [];
+  renderTasks();
+});
+
+function saveTasks() {
+  chrome.storage.sync.set({
+    tasks,
+  });
+}
 
 function renderTask(taskNum) {
   const taskRow = document.createElement("div");
@@ -12,6 +67,7 @@ function renderTask(taskNum) {
   text.value = tasks[taskNum];
   text.addEventListener("change", () => {
     tasks[taskNum] = text.value;
+    saveTasks();
   });
 
   const deleteBtn = document.createElement("input");
@@ -32,11 +88,13 @@ function addTask() {
   const taskNum = tasks.length;
   tasks.push("");
   renderTask(taskNum);
+  saveTasks();
 }
 
 function deleteTask(taskNum) {
   tasks.splice(taskNum, 1);
   renderTasks();
+  saveTasks();
 }
 
 function renderTasks() {
